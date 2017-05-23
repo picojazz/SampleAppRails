533873cbe0d2e111de4098d5ebc6bd0bfd1fb451
picojazz <picojazzz@gmail.com>
2017-05-23 15:33:29 +0000

    add basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'session/new'
+
   get '/signup',to: 'users#new'
   root 'static_pages#home'
   get '/contact',to: 'static_pages#contact'
   get '/about',to: 'static_pages#about'
   get '/help',to: 'static_pages#help'
-  resources :users
   post '/signup' ,to: 'users#create'
+  get '/login', to:'sessions#new'
+  post '/login',to:'sessions#create'
+  delete '/logout',to:'sessions#destroy' 
+
+
+  resources :users
 
 
 
